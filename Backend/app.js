5384c3a261f2889698161fddfcf2dfd1a6674361
mkzmohanad <mkzmohanad@gmail.com>
2024-11-03 15:41:57 +0200

    test-5

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ const settingsRoutes = require('./Routes/SettingsRoutes');
 const app = express();
 
 const corsOptions = {
-    origin: "mkz-quiz-frontend-drab.vercel.app", // Your frontend origin
+    origin: "https://mkz-quiz-frontend-drab.vercel.app", // Your frontend origin
     methods : ['GET', 'POST', 'PATCH' , 'DELETE'],
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 };
