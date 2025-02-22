1d67062586b01046686e4a60f34720541c175932
mkzmohanad <mkzmohanad@gmail.com>
2025-02-22 21:47:06 +0200

    try

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,21 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures correct asset paths
-  server: {
-    port: 3000, // Uses Railway's port dynamically
-  },
-  build: {
-    outDir: 'dist', 
-    minify: 'esbuild', // Faster & less memory usage than 'terser'
-    target: 'esnext', // Prevents unnecessary polyfills
-    chunkSizeWarningLimit: 1500, // Avoids chunk splitting warnings
-    rollupOptions: {
-      output: {
-        manualChunks: undefined, // Reduces chunk fragmentation
-      },
-    },
-  }
-});
+})
